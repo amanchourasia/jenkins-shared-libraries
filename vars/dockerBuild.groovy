773d947f77a440bbe324ac909147868a2957faa3
amanchourasia <40295937+amanchourasia@users.noreply.github.com>
2019-09-27 03:10:36 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,11 +1,3 @@
 def call(String project, String hubUser) {
     sh "sudo docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
-    withCredentials([usernamePassword(
-            credentialsId: "dockerhub",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
-        sh "sudo docker login -u '$USER' -p '$PASS'"
-    }
-    sh "sudo docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
 }
