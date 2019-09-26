4bbb8abeaa6a6305c7fde011dd35c2a89ef8ac8d
amanchourasia <40295937+amanchourasia@users.noreply.github.com>
2019-09-27 03:40:51 +0530

    Update dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -6,5 +6,5 @@ def call(String project, String hubUser) {
     )]) {
         sh "sudo docker login -u '$USER' -p '$PASS'"
     }
-    sh "sudo docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+    sh "sudo docker image push ${hubUser}/${project}:latest"
 }
