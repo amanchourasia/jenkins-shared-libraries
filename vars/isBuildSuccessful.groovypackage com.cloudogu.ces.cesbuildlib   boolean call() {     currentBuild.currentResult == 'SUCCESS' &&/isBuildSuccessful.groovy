6a49925ee26a0e27fd75abcd4adca491a82801e8
amanchourasia <40295937+amanchourasia@users.noreply.github.com>
2019-09-27 03:27:28 +0530

    Delete isBuildSuccessful.groovy

--- vars/isBuildSuccessful.groovypackage com.cloudogu.ces.cesbuildlib   boolean call() {     currentBuild.currentResult == 'SUCCESS' &&/isBuildSuccessful.groovy
+++
@@ -1,8 +0,0 @@
-package com.cloudogu.ces.cesbuildlib
-
-
-boolean call() {
-    currentBuild.currentResult == 'SUCCESS' &&
-        // Build result == SUCCESS seems not to set be during pipeline execution.
-        (currentBuild.result == null || currentBuild.result == 'SUCCESS')
-}
